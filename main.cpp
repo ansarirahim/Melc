15a3bffc9debd2c58d26608ac569721b3064024a
root <root@beaglebone.localdomain>
2020-06-29 16:36:12 +0000

    first commit

---
+++ main.cpp
@@ -0,0 +1,50 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define NO_OF_COL 44
+#include <stdint.h>
+int countlines(char *filename);
+
+int  main(int argc, char *argv[])
+{
+int  rows=countlines("Flash.HEX");
+printf("LINES: %d\n",rows);
+char *p{ new int [rows*NO_OF_COL]{} };
+//char *p = new (char)*(rows*NO_OF_COL);/// sizeof(char) * ( rows + 1 ) );
+//char (*orderedIds)[NO_OF_COL+1]= malloc(rows * sizeof(*orderedIds));
+FILE *fp = fopen("Flash.HEX","r");;
+int i=0;
+while (fgets(p,rows,fp)!=NULL)
+{
+
+printf("\n%d=%s",i,p);
+i++;}
+//free(p);//orderedIds);
+}    //int *arr = (int *)malloc(rows * NO_OF_COL * sizeof(int)); 
+
+
+
+
+
+//Open file for reading, function.
+int countlines(char *filename)
+{
+  // count the number of lines in the file called filename                                    
+  FILE *fp = fopen(filename,"r");
+  int ch=0;
+  int lines=0;
+
+  if (fp == NULL)
+  return 0;
+
+  lines++;
+  while ((ch = fgetc(fp)) != EOF)
+    {
+      if (ch == '\n')
+    lines++;
+    }
+  fclose(fp);
+  return lines;
+
+
+
+}
